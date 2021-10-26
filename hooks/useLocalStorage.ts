21c0e0d3c0c9cf957464b28809eb8763c4dbac1b
Jason Addleman <jas7457@gmail.com>
2021-10-26 17:42:01 -0400

    Some changes to useLocaleStorage to know when it has been set

--- hooks/useLocalStorage.ts
+++ hooks/useLocalStorage.ts
@@ -1,10 +1,16 @@
 import { useState, useEffect, Dispatch, SetStateAction } from 'react';
 
-export default function useLocalStorage<T>(key: string, init: T | (() => T)): [T, Dispatch<SetStateAction<T>>] {
+export default function useLocalStorage<T>(
+	key: string,
+	init: T | (() => T)
+): [T, Dispatch<SetStateAction<T>>, boolean] {
 	const [state, setState] = useState(init);
+	const [wasLoaded, setWasLoaded] = useState(false);
 
 	// in an effect so we ensure we're on the client, not on the server
 	useEffect(() => {
+		setWasLoaded(true);
+
 		try {
 			const val = localStorage.getItem(key);
 			if (val !== null) {
@@ -17,5 +23,5 @@ export default function useLocalStorage<T>(key: string, init: T | (() => T)): [T
 		localStorage.setItem(key, JSON.stringify(state));
 	}, [key, state]);
 
-	return [state, setState];
+	return [state, setState, wasLoaded];
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,13 +6,20 @@ import DarkModeContext from '../context/DarkModeContext';
 import useLocalStorage from '../hooks/useLocalStorage';
 
 export default function App({ Component, pageProps }: AppProps) {
-	const [isDarkMode, setIsDarkMode] = useLocalStorage('darkMode', false);
+	const [isDarkMode, setIsDarkMode, wasLoaded] = useLocalStorage<boolean>('darkMode', undefined!);
 	const [shouldShow, setShouldShow] = useState(false);
 
 	useEffect(() => {
-		setIsDarkMode(window.matchMedia?.('(prefers-color-scheme: dark)').matches ?? false);
+		if (!wasLoaded) {
+			return;
+		}
+
+		if (isDarkMode === undefined) {
+			setIsDarkMode(window.matchMedia?.('(prefers-color-scheme: dark)').matches ?? false);
+		}
+
 		setShouldShow(true);
-	}, [setIsDarkMode]);
+	}, [setIsDarkMode, wasLoaded, isDarkMode]);
 
 	if (!shouldShow) {
 		return null;
